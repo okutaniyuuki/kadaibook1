6f7be4a4fd0d48cd8e9bbb4f1e3b4b85b325cf09
okutaniyuuki <oy085276@gmail.com>
2020-04-19 13:42:46 +0900

    sss

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  def index
+  	@book  =  Book.new
+  	 @books = Book.all
+  end
+
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+   @book =  Book.new
+  end
+
+  def create  
+  	book = Book.new(book_params)
+       if book.save
+       flash[:notice] = 'Book was successfully created.'
+
+
+
+        redirect_to book_path(book.id)
+      else
+        @books = Book.all
+        @book = book
+        render "index"
+
+       end
+  end
+
+
+  def edit
+     @book = Book.find(params[:id])
+  end
+
+  def  update
+   
+    book = Book.find(params[:id])
+    book.update(book_params)
+    
+       if book.save
+        flash[:notice] = 'Book was successfully created.'
+         
+
+
+       else
+        @book = Book.all
+        @book = book
+        render "edit"
+      end
+ end
+   def  destroy
+     book =Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+   end
+
+
+   private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
